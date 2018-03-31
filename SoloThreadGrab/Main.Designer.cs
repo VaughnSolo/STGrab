4d8ad88845d6c8fbd1ca90b0f2ae387c9fd51476
James Stumpf <james.v.stumpf@gmail.com>
2018-03-31 14:52:24 -0400

    Threadname grabber and file URL grabber completed.

--- SoloThreadGrab/Main.Designer.cs
+++ SoloThreadGrab/Main.Designer.cs
@@ -29,33 +29,44 @@
         private void InitializeComponent()
         {
             this.buttonGrab = new System.Windows.Forms.Button();
+            this.textNewURL = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonGrab
             // 
-            this.buttonGrab.Location = new System.Drawing.Point(85, 52);
+            this.buttonGrab.Location = new System.Drawing.Point(197, 12);
             this.buttonGrab.Name = "buttonGrab";
-            this.buttonGrab.Size = new System.Drawing.Size(75, 23);
+            this.buttonGrab.Size = new System.Drawing.Size(75, 20);
             this.buttonGrab.TabIndex = 0;
             this.buttonGrab.Text = "Start";
             this.buttonGrab.UseVisualStyleBackColor = true;
             this.buttonGrab.Click += new System.EventHandler(this.buttonGrab_Click);
+            // 
+            // textNewURL
+            // 
+            this.textNewURL.Location = new System.Drawing.Point(12, 12);
+            this.textNewURL.Name = "textNewURL";
+            this.textNewURL.Size = new System.Drawing.Size(179, 20);
+            this.textNewURL.TabIndex = 1;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textNewURL);
             this.Controls.Add(this.buttonGrab);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonGrab;
+        private System.Windows.Forms.TextBox textNewURL;
     }
 }
 
--- SoloThreadGrab/Main.cs
+++ SoloThreadGrab/Main.cs
@@ -5,21 +5,57 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 namespace SoloThreadGrab
 {
     public partial class Main : Form
     {
+        private List<string> itemsToGrab;
         public Main()
         {
             InitializeComponent();
         }
-
+        // Inintiate New Thread Download
         private void buttonGrab_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Git, post VS Update.");
+            WebClient client = new WebClient();
+            client.UseDefaultCredentials = true;
+            client.Headers.Add("User-Agent: Other");
+            string downloadString = client.DownloadString(textNewURL.Text);
+            findThreadName(downloadString);
+            itemsToGrab = findItems(downloadString);
+        }
+        // Get Thread Name For Folder
+        private string findThreadName(string fetchedText)
+        {
+            string ret;
+            string titleMarker = "<meta name=\"description\" content=\"";
+            string endMarker = " - &quot;/";
+            int titlePos = fetchedText.IndexOf(titleMarker) + titleMarker.Length;
+            int titlePosEnd = fetchedText.IndexOf(endMarker);
+            ret = fetchedText.Substring(titlePos, titlePosEnd - titlePos);
+            return ret;
+        }
+        // Get List of All Item URLs
+        private List<string> findItems(string fetchedText)
+        {
+            int fileCount = 0;
+            List<string> ret = new List<string>();
+            string test = "";
+            Regex fileRegex = new Regex("(?:a title=.*? href|a href)=\"\\/\\/(.{1,50}?\\.(?:gif?|webm))");
+            foreach (Match match in fileRegex.Matches(fetchedText))
+            {
+                fileCount++;
+                ret.Add(match.Groups[1].Value);
+                test += match.Groups[1].Value + "\r\n";
+            }
+            MessageBox.Show(test);
+            MessageBox.Show(fileCount.ToString());
+            return ret;
         }
     }
 }
